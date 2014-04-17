d512d149a5c3e7e6ccf1ffe285ad9553b4c24e86
dcsan <dc@pikkle.com>
2014-04-16 18:53:09 -0700

    mods

--- bin/import.sh
+++ bin/import.sh
@@ -1,4 +1,7 @@
-mongoimport -h localhost:3001 --db meteor --type csv --drop --headerline --collection players --file public/logs/players.csv
+
+## make sure you're not using a remote DB if using '--drop'
+unset MONGO_URL
+mongoimport -h localhost:3001 --db meteor --type csv --drop --headerline --collection diamonds --file public/logs/diamonds-use.csv
 
 ### from json
 # mongoimport -h localhost:3001 --db meteor --collection users --file public/logs/users.json
--- bin/local.sh
+++ bin/local.sh
@@ -2,6 +2,6 @@
 # export MONGO_URL="mongodb://heroku_app15780382:12cdlj4vrch0kdt7lsdouhs04q@ds027339-a0.mongolab.com:27339,ds027339-a1.mongolab.com:27339/heroku_app15780382"
 
 ## staging test db
-export MONGO_URL="mongodb://heroku2:XG7ETzAUfdouWLAgn9QwirsiQLHDvc@dharma.mongohq.com:10010/app11351866"
+# export MONGO_URL="mongodb://heroku2:XG7ETzAUfdouWLAgn9QwirsiQLHDvc@dharma.mongohq.com:10010/app11351866"
 
 # unset MONGO_URL
