59e7a175b3ef0fb028cddbf0ade72768545b9436
dc <dc+github@biz.pikkle.com>
2015-07-20 11:19:33 -0700

    Update local.sh

--- bin/local.sh
+++ bin/local.sh
@@ -1,3 +1,5 @@
+## no, these are not live anymore 
+
 ## production mongo
 # export MONGO_URL="mongodb://heroku_app15780382:12cdlj4vrch0kdt7lsdouhs04q@ds027339-a0.mongolab.com:27339,ds027339-a1.mongolab.com:27339/heroku_app15780382"
 
